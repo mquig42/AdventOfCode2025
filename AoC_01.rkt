8b813b48e4c1436660fb1f98850a6cbb8a70f03a
mquig42 <mquig42@users.noreply.github.com>
2025-12-01 18:26:11 -0500

    Refactored day 1

--- AoC_01.rkt
+++ AoC_01.rkt
@@ -3,41 +3,37 @@
 ;;;Mike Quigley
 
 ;;;And we're off!
-;;;This is a quick and dirty program that solves both parts of today's problem
-;;;solve1 and solve2 are nearly identical and should be refactored
-;;;and it may be better to have an input parsing function that can be mapped
-;;;over the raw output from file->lines
+;;;Refactored my original solution. The solve function is much simpler.
+;;;count-zero-2 is still inefficient. It's fast enough, but constant time
+;;;should be possible.
 #lang racket
 
-(define (solve1 pos count lst)
-  (if (null? lst) count
-      (let* ((op (if (eq? (string-ref (car lst) 0) #\R) + -))
-             (dist (string->number (substring (car lst) 1)))
-             (newpos (modulo (op pos dist) 100)))
-        (solve1 newpos
-                (if (eq? newpos 0) (add1 count) count)
-                (cdr lst)))))
+(define (parse-input line)
+  (cons (if (eq? (string-ref line 0) #\R) + -)
+        (string->number (substring line 1))))
 
-(define (solve2 pos count lst)
+(define (solve count-zero pos count lst)
   (if (null? lst) count
-      (let* ((op (if (eq? (string-ref (car lst) 0) #\R) + -))
-             (dist (string->number (substring (car lst) 1)))
-             (newpos (modulo (op pos dist) 100)))
-        (solve2 newpos
-                (+ (count-zero op pos dist 0) count)
-                (cdr lst)))))
+      (solve count-zero
+             (modulo ((caar lst) pos (cdar lst)) 100)
+             (+ count (count-zero (caar lst) pos (cdar lst) 0))
+             (cdr lst))))
+
+;Count the number of times the dial ends at zero
+(define (count-zero-1 op pos dist count)
+  (if (zero? (modulo (op pos dist) 100)) 1 0))
 
 ;Count the number of times the dial passes zero
-(define (count-zero op pos dist count)
+(define (count-zero-2 op pos dist count)
   (if (eq? dist 0) count
       (let ((newpos (modulo (op pos 1) 100)))
-        (count-zero op newpos (sub1 dist)
-                    (if (zero? newpos) (add1 count) count)))))
+        (count-zero-2 op newpos (sub1 dist)
+                      (if (zero? newpos) (add1 count) count)))))
                   
 
-(define input (file->lines "Input01.txt"))
+(define input (map parse-input (file->lines "Input01.txt")))
 
 (display "Part 1: ")
-(solve1 50 0 input)
+(solve count-zero-1 50 0 input)
 (display "Part 2: ")
-(solve2 50 0 input)
+(solve count-zero-2 50 0 input)
