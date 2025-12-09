bb1828a6adb1183bc8759c0bd658b3e08af8f8df
mquig42 <mquig42@users.noreply.github.com>
2025-12-08 21:56:06 -0500

    Find shortest distances

--- AoC_08.rkt
+++ AoC_08.rkt
@@ -3,7 +3,29 @@
 ;;;Mike Quigley
 #lang racket
 
-(define (process-line line)
-  (map string->number (string-split line ",")))
+;Generates a list of all possible pairs of elements in lst
+(define (enumerate-all-pairs lst)
+  (define (iter lst-a lst-b acc)
+    (cond ((null? (cdr lst-a)) acc)
+          ((null? lst-b)
+           (iter (cdr lst-a) (cdr (cdr lst-a)) acc))
+          (else
+           (iter lst-a (cdr lst-b) (cons (list (car lst-a) (car lst-b)) acc)))))
+  (iter lst (cdr lst) null))
 
-(define input (map process-line (file->lines "Test08.txt")))
+;Calculates the distance between two points
+(define (distance pair)
+  (sqrt (+ (expt (- (first (first pair)) (first (second pair))) 2)
+           (expt (- (second (first pair)) (second (second pair))) 2)
+           (expt (- (third (first pair)) (third (second pair))) 2))))
+
+;Finds the n pairs which are closest together
+(define (shortest-distances lst n)
+  (take (sort lst (λ (a b) (< (distance a) (distance b)))) n))
+
+;(define input (map process-line (file->lines "Test08.txt")))
+(define input
+  (map (λ (x) (map string->number (string-split x ",")))
+       (file->lines "Test08.txt")))
+
+(shortest-distances (enumerate-all-pairs input) 10)
