92ccae3bdf9027b3103efd54d8583855b57ef5e0
mquig42 <mquig42@users.noreply.github.com>
2025-12-11 20:56:02 -0500

    Day 11 part 2

--- AoC_11.rkt
+++ AoC_11.rkt
@@ -1,7 +1,10 @@
 ;;;AoC_11.rkt
 ;;;2025-12-11
 ;;;Mike Quigley
+
+;;;Memoization can be a powerful thing. So can hash maps.
 #lang racket
+(require memo)
 
 ;Read input into a hash map
 (define (read-input lines acc)
@@ -11,19 +14,18 @@
                             (substring (car lines) 0 3)
                             (drop (string-split (car lines) " ") 1)))))
 
-(define (merge-lists a b)
-  (if (null? a) b
-      (merge-lists (cdr a) (cons (car a) b))))
-
-(define (count-paths stack acc)
-  (cond ((null? stack) acc)
-        ((string=? (car stack) "out")
-         (count-paths (cdr stack) (add1 acc)))
-        (else (count-paths
-               (merge-lists (hash-ref input (car stack)) (cdr stack))
-               acc))))
+;Using memoize because the number of paths is very large
+;dac and fft are bools which track whether we've passed them on our current path
+(define/memoize (count-paths start dac fft)
+  (if (and (string=? start "out") dac fft) 1
+      (foldl + 0
+             (map (λ (x) (count-paths x (or dac (string=? start "dac"))
+                                      (or fft (string=? start "fft"))))
+                  (hash-ref input start null)))))
 
 (define input (read-input (file->lines "Input11.txt") (hash)))
 
 (display "Part 1: ")
-(count-paths '("you") 0)
+(count-paths "you" true true)
+(display "Part 2: ")
+(count-paths "svr" false false)
