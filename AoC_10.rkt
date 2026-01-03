a16b146051298fb98fc94ba77ffc3374691e3797
mquig42 <mquig42@users.noreply.github.com>
2026-01-03 15:25:38 -0500

    Day 10 part 1

--- AoC_10.rkt
+++ AoC_10.rkt
@@ -8,6 +8,7 @@
 ;;;section of each line to weight the moves. Dijkstra could solve that.
 ;;;This is the year I finally make a fully-general Dijkstra solver
 #lang racket
+(require data/heap)
 
 ;;Machine description:
 ;;1. Goal state
@@ -39,6 +40,14 @@
           (map parse-button (drop-right (cdr sp) 1))
           (extract-numbers (last sp)))))
 
+;Getters
+(define (goal machine)
+  (first machine))
+(define (buttons machine)
+  (second machine))
+(define (joltages machine)
+  (third machine))
+
 ;For debug purposes, convert an integer representation of the panel lights
 ;into a string, using '.' for off and '#' for on
 ;Does not include trailing offs
@@ -55,4 +64,54 @@
            (iter lights (/ pow2 2) (cons #\. lst)))))
   (list->string (iter lights (largest-pow2 lights 1) null)))
 
-(define input (map parse-machine (file->lines "Test10.txt")))
+(define (enumerate-moves machine state)
+  (map (λ (x) (cons (add1 (car state)) (bitwise-xor (cdr state) x)))
+       (buttons machine)))
+
+;;Queue functions
+;Creates a queue element, which has a priority and value
+(define (q-element priority value)
+  (cons priority value))
+;Compare priority of two queue elements
+(define (q<= a b)
+  (<= (car a) (car b)))
+;Changes an element's priority
+(define (q-reduce-priority! q value from to)
+  (heap-remove-eq! q (q-element from value))
+  (heap-add! q (q-element to value)))
+;Returns value of element with lowest priority and removes it from queue
+(define (q-lowest! q)
+  (let ((lowest (heap-min q)))
+    (heap-remove-min! q)
+    lowest))
+
+(define (dijkstra grid start goal? enumerate-moves)
+  (let ((unvisited (make-heap q<=))
+        (distances (make-hash)))
+    (define (visit node)
+      (let ((dist (car node))
+            (state (cdr node)))
+        (cond ((goal? state) dist)
+              (else
+               (for-each (λ (x)
+                           (let ((old-dist (hash-ref distances (cdr x) +inf.0))
+                                 (new-dist (car x)))
+                             (cond ((> old-dist new-dist)
+                                    (hash-set! distances (cdr x) new-dist)
+                                    (q-reduce-priority!
+                                     unvisited (cdr x) old-dist new-dist)))))
+                         (filter (λ (x) (not (hash-has-key? distances (cdr x))))
+                                 (enumerate-moves grid node)))
+               (hash-set! distances state dist)
+               (visit (q-lowest! unvisited))))))
+    (heap-add! unvisited (q-element 0 start))
+    (hash-set! distances start 0)
+    (visit (q-lowest! unvisited))))
+
+(define input (map parse-machine (file->lines "Input10.txt")))
+
+(display "Part 1: ")
+(foldl + 0
+       (map (λ (machine)
+              (dijkstra machine 0 (λ (x) (= x (car machine))) enumerate-moves))
+            input))
